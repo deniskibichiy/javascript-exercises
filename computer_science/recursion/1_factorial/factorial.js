52936495f898d0f35fa338366abd42a72a662f0f
Denis Kibichiy <deniskibichiy@outlook.com>
2026-05-30 16:19:54 +0300

    implement 01_factorial

--- computer_science/recursion/1_factorial/factorial.js
+++ computer_science/recursion/1_factorial/factorial.js
@@ -1,5 +1,22 @@
-const factorial = function() {
+const factorial = function(number, product = 1) {
+    if (typeof number !== "number" || !Number.isInteger(number)) {
+    return undefined;
+}
 
+if (number < 0) {
+    return undefined;
+}
+
+
+    if (number == 1 || number == 0) {
+        return product;
+    } else {
+        
+        product = product * number;
+        number = number - 1;
+        debugger;
+        return factorial(number, product)
+    }
 };
 
 // Do not edit below this line
